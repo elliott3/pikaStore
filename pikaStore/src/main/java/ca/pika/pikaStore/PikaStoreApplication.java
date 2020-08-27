239428f059754c24eeab682fb6ab124b7cd9e859
Elliott3 <32347682+elliott3@users.noreply.github.com>
2020-08-27 16:09:42 -0400

    Add files via upload

---
+++ pikaStore/src/main/java/ca/pika/pikaStore/PikaStoreApplication.java
@@ -0,0 +1,13 @@
+package ca.pika.pikaStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PikaStoreApplication.class, args);
+	}
+
+}
---
+++ pikaStore/src/test/java/ca/pika/pikaStore/PikaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.pika.pikaStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
